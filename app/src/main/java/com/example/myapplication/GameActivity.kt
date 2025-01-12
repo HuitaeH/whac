66044114945d7c663518f8acde57b56ba9e4aafb
HuitaeH <huitaehanww@kaist.ac.kr>
2025-01-12 17:28:06 +0900

    randombug

--- app/src/main/java/com/example/myapplication/GameActivity.kt
+++ app/src/main/java/com/example/myapplication/GameActivity.kt
@@ -16,18 +16,24 @@ class GameActivity : AppCompatActivity() {
         // Set the layout file where the ImageView is defined
         setContentView(R.layout.game_activity)
 
-        val ant: ImageView = findViewById(R.id.ant)
-
+        // Randomly choose an image (ant, grasshopper, or worm)
+        val bug: ImageView = findViewById(R.id.ant) // Access the ImageView correctly
+        val bugType = Random.nextInt(3) // 0, 1, or 2
+        when (bugType) {
+            0 -> bug.setImageResource(R.drawable.ant) // Set image to ant
+            1 -> bug.setImageResource(R.drawable.grasshopper) // Set image to grasshopper
+            2 -> bug.setImageResource(R.drawable.worm) // Set image to worm
+        }
         // Get screen width
         val screenWidth = resources.displayMetrics.widthPixels
         val screenHeight = resources.displayMetrics.heightPixels
 
         // Set a random Y position for the bug
-        val randomY = Random.nextInt(0, screenHeight - ant.layoutParams.height)
-        ant.y = randomY.toFloat()
+        val randomY = Random.nextInt(0, screenHeight - bug.layoutParams.height)
+        bug.y = randomY.toFloat()
 
         // Animate the bug from right to left
-        val animator = ObjectAnimator.ofFloat(ant, "translationX", screenWidth.toFloat(), -screenWidth.toFloat())
+        val animator = ObjectAnimator.ofFloat(bug, "translationX", screenWidth.toFloat(), -screenWidth.toFloat())
         animator.duration = 5000L // Duration in milliseconds (3 seconds)
         animator.start()
     }
