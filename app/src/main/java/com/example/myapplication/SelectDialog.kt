6a03a23ec63583dfeff3f501b553d5cbfe8c90ad
Madcamp2 <navygrace8389@gmail.com>
2025-01-12 17:06:22 +0900

    dialog

--- app/src/main/java/com/example/myapplication/SelectDialog.kt
+++ app/src/main/java/com/example/myapplication/SelectDialog.kt
@@ -3,27 +3,59 @@ import android.app.AlertDialog
 import android.app.Dialog
 import android.content.DialogInterface
 import android.content.Intent
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.view.WindowManager
+import android.widget.Button
+import androidx.core.content.res.ResourcesCompat
 import androidx.fragment.app.DialogFragment
 
 
 class SelectDialog : DialogFragment() {
-    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
-        return AlertDialog.Builder(requireContext())
-            .setTitle("Game Mode")
-            .setMessage("Select your game mode!")
-            .setPositiveButton("Single") { dialog: DialogInterface?, which: Int ->
-                // Launch GameActivity for single-player mode
-                val intent = Intent(requireContext(), GameActivity::class.java)
-                startActivity(intent)
-                dialog?.dismiss() // Optionally dismiss the dialog
-            }
-            .setNegativeButton("Double") { dialog: DialogInterface?, which: Int ->
-                // Launch FriendListActivity for double-player mode
-                val intent = Intent(requireContext(), FriendListActivity::class.java)
-                startActivity(intent)
-                dialog?.dismiss() // Optionally dismiss the dialog
-            }
-            .create()
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        dialog?.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+        return inflater.inflate(R.layout.select_dialog, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        view.findViewById<Button>(R.id.singleButton).setOnClickListener {
+            dismiss()
+        }
+
+        view.findViewById<Button>(R.id.doubleButton).setOnClickListener {
+            val intent = Intent(requireContext(), FriendListActivity::class.java)
+            startActivity(intent)
+            dismiss()
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        dialog?.window?.apply {
+            setLayout(
+                WindowManager.LayoutParams.MATCH_PARENT,
+                WindowManager.LayoutParams.WRAP_CONTENT
+            )
+
+            setBackgroundDrawable(
+                ResourcesCompat.getDrawable(
+                    resources,
+                    R.drawable.dialog_background,
+                    null
+                )
+            )
+        }
     }
 }
+
